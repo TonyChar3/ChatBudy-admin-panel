3acd3608ac3219fdf4763083c660f412b18a2cf4
Anthony <charetteanthony@outlook.com>
2023-11-04 09:57:10 -0400

    Fixed a styling issue where the loader for when the user register was still using the old deleted images

--- src/context/Loader/Register_loading/spinningLoader.jsx
+++ src/context/Loader/Register_loading/spinningLoader.jsx
@@ -15,20 +15,20 @@ const SpinningLoaderPage = () => {
                 exit={{ opacity: 0, transition: { duration: 0.6 } }}
             >
                 <div className="absolute top-[5%] right-10 lg:right-60 lg:top-[10%]">
-                    <img src="/images/4.png" alt="money cnava" width="40" height="40" className="rounded-full"/>
+                    <div className="w-[40px] h-[40px] bg-[#6C2E9C] rounded-full"></div>
                 </div>
                 <div className="absolute top-[10%] left-10 lg:top-[20%] lg:left-20">
-                    <img src="/images/4.png" alt="money cnava" width="100" height="100" className="rounded-full"/>
+                    <div className="w-[100px] h-[100px] bg-[#6C2E9C] rounded-full"></div>
                 </div>
                 <div className="w-[70%] h-[33%] lg:w-[20%] lg:h-[40%] border-t-2 border-[#33b8b8] border-solid rounded-full animate-spin"></div>
                 <div className="absolute flex flex-col justify-center items-center animate-growAndShrink duration-2000 ease-in-out infinite">
                     <i className="fa-light fa-comments text-6xl lg:text-8xl text-[#33b8b8]"></i>
                 </div>
                 <div className="absolute bottom-[5%] right-10 lg:bottom-[10%] lg:right-[15%]">
-                    <img src="/images/4.png" alt="money cnava" width="60" height="60" className="rounded-full"/>
+                    <div className="w-[60px] h-[60px] bg-[#6C2E9C] rounded-full"></div>
                 </div>
                 <div className="absolute bottom-20 left-10 lg:left-[10%] lg:bottom-[15%] rounded-xl">
-                    <img src="/images/1.png" alt="chat canva" width="100" height="100" className="rounded-xl"/>
+                    <img src="https://res.cloudinary.com/dskpbps9l/image/upload/v1698260563/ChatBudy.io/Increase_Your_Sales_16_mfgnnp.png" width="150" height="150" alt="canva image" className="rounded-xl" />
                 </div>
             </motion.div>
         </>
