a6bec1991628d13f8fba709b17a7e3535043c884
Tony Char3 <tonyc6731@gmail.com>
2024-08-25 08:02:26 -0400

    FIX: Analytics not loading properly due to chart.js elements not registered to make the chart appear with react-chart-js

--- src/app/admin-panel/pages/analytics/components/linear-chart.component.jsx
+++ src/app/admin-panel/pages/analytics/components/linear-chart.component.jsx
@@ -1,6 +1,14 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+} from "chart.js";
 import { Line } from "react-chartjs-2";
 
 const LinearChartComponent = ({ chartData }) => {
+  ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement);
   return (
     <>
       <Line
